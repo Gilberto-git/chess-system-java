02e203c1a9853babbff2b1f498e2bb18e1e5fc3e
Gilberto de Freitas Lins <dilbert.gil@gmail.com>
2021-04-16 15:48:20 -0300

    teste

---
+++ src/appliacation/program.java
@@ -0,0 +1,17 @@
+package appliacation;
+
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		 String x  ;
+		 Scanner sc = new Scanner(System.in);
+		System.out.println("hello, World, tecle ENTER");
+		x =sc.nextLine();
+		sc.close();
+		System.out.println("fim");
+		System.out.println(x);
+	}
+
+}
